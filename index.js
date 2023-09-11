b0dfebe9cd8a93770f8d1d68819a2471f15cd011
dynamic109 <120388304+dynamic109@users.noreply.github.com>
2023-09-11 18:24:51 +0100

    Add files via upload
    i updated my html and javascript

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const currentUtcTime = document.querySelector('[data-testid="currentUTCTime"]')
-const currentDayOfTheWeek = document.querySelector('[data-testid="currentDayOfTheWeek"]')
+const currentUtcTime = document.querySelector('[data-testid="currentUTCTime"]');
+const currentDayOfTheWeek = document.querySelector('[data-testid="currentDayOfTheWeek"]');
 const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
 function getDay(){
-    const date = new Date()
-    const day = weekday[date.getDay()]
-    currentDayOfTheWeek.textContent = `${day}, `
+    const date = new Date();
+    const day = weekday[date.getDay()];
+    currentDayOfTheWeek.textContent = `${day}, `;
 }
 
 function getTimeInMilliseconds(){
 
-    const date = new Date()
-    const time = date.getTime()
-    currentUtcTime.textContent =  time
+    const date = new Date();
+    const time = date.getTime();
+    currentUtcTime.textContent =  time;
 
 }
-getDay()
-setInterval( getTimeInMilliseconds, 1000)
+getDay();
+setInterval( getTimeInMilliseconds, 1000);
 
